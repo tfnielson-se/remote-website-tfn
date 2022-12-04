c3c7016e692b1c508887c6578d6f0e7c9b24c6cf
Thomas <tfnielson.se@gmail.com>
2022-12-04 12:41:11 -0700

    reassigning files

---
+++ script-tester.js
@@ -0,0 +1,39 @@
+// CODE HERE
+
+
+
+
+/*
+// ADDING ABOUT ME H2
+const aboutMe = document.createElement("h2");
+aboutMe.textContent = "About Me";
+aboutMe.setAttribute("id", "about-me");
+aboutMe.setAttribute("class", "me-header");
+
+document.querySelector("body").appendChild(aboutMe);
+
+// ADDING TEXT TO <P> ABOUT ME SECTION
+const aboutMeInfo = document.createElement("p");
+aboutMeInfo.setAttribute("id", "about-me-info");
+aboutMeInfo.setAttribute("class", "me-infotext");
+aboutMeInfo.textContent = " // add info about me"
+
+document.body.appendChild(aboutMeInfo);
+
+// ADDIND CONTACT ME H2
+const contactMe = document.createElement("h2");
+contactMe.textContent = "Contact Me";
+contactMe.setAttribute("id", "contact-me");
+contactMe.setAttribute("class", "me-header");
+
+document.querySelector("body").appendChild(contactMe);
+
+// ADDING TEXT TO <P> CONTACT ME SECTION
+const contactMeInfo = document.createElement("p");
+contactMeInfo.setAttribute("id", "contact-me-info");
+contactMeInfo.setAttribute("class", "me-infotext");
+contactMeInfo.textContent = " // add info contact me"
+
+document.body.appendChild(contactMeInfo);
+*/
+
